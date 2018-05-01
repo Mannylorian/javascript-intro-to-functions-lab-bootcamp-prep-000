8d5d8b08709033a4f2f0b726f5bc2e2d9fd215f8
Emmanuel Anderson <manu1526@gmail.com>
2018-05-01 13:54:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var uppercase, lowercase;
 
 function shout(string){
@@ -30,3 +31,31 @@ function sayHiToGrandma(string){
   return "I love you, too.";
 }
 }
+=======
+var uppercase='HELLO!'
+function shout(string){
+  return string.toUpperCase()
+}
+
+function logShout(string){
+  console.log ("HELLO!");
+}
+
+function logWhisper(string){
+  console.log ("hello!");
+}
+
+var lowercase ='hello!'
+function whisper(string){
+  return string.toLowerCase()
+}
+lowercase.toLowerCase()===lowercase;
+uppercase.toUpperCase()===uppercase;
+function sayHiToGrandma(string){
+  if ('string' === "I love you, Grandma");
+  return "I love you, too."
+} else if {
+  ('string'=== lowercase)
+}
+
+>>>>>>> c6a0f71297e803d706ff99a3a685bc8088f1a174
